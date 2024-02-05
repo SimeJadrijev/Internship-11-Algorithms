3eebeaff01786d95ad24cf1aca918cbe9b67e367
SimeJadrijev <sime.jadrijev@gmail.com>
2024-02-05 18:45:02 +0100

    Finished the 4th task. Skipped previous ones for now

--- index.js
+++ index.js
@@ -4,42 +4,148 @@
 // izračunati prosječnu cijenu i ispisati proizvod koji najviše 
 // odstupa od prosjeka
 
-const articles = [];
-while (true) 
-{
-    const name = prompt("Unesite ime proizvoda");
-    const price = +prompt("Unesite cijenu proizvoda");
-    const color = prompt("Unesite boju proizvoda:");
+// const articles = [];
+// while (true) 
+// {
+//     const name = prompt("Unesite ime proizvoda");
+//     const price = +prompt("Unesite cijenu proizvoda");
+//     const color = prompt("Unesite boju proizvoda:");
     
-    const article = {
-        name, 
-        price, 
-        color
+//     const article = {
+//         name, 
+//         price, 
+//         color
+//     };
+
+//     articles.push(article);
+
+//     const continuation = confirm("Želite li nastaviti unos?");
+
+//     if (continuation === false)
+//         break;
+// }
+
+// const sumOfAllPrices = articles.reduce((acc, value) => acc + value.price, 0);
+// const averagePrice = (sumOfAllPrices / articles.length).toFixed(2);
+
+
+// let maxDeviation = 0;
+// let maxDeviationArticle = articles[0];
+// articles.forEach(a => {
+//     const deviation = Math.abs(a.price - averagePrice);
+
+//     if (deviation > maxDeviation) {
+//         maxDeviation = deviation;
+//         maxDeviationArticle = a;
+//     }
+// });
+
+// console.log("Prosjek cijena: ", averagePrice);
+// console.log("Najveće odstupanje: ", maxDeviationArticle, " (", maxDeviation.toFixed(2), " eura)");
+
+// ------------------------------------------------------------------------------------------------------
+
+// 2. Korisnik redom upisuje ime, prezime, zanimanje i plaću osobe. 
+// Unos se obavlja isto kao u prvom zadatku (tako i u ostalim zadacima ovog tipa). 
+// Cilj zadatka je izračunati prosjek plaće za svako zanimanje i ispisati 
+// sortirano objekte gdje piše zanimanje, prosjek i koliko osoba radi to zanimanje.
+
+// const people = [];
+// const dictionary = [];
+
+// while (true) {
+
+//     const name = prompt("Unesite ime: ");
+//     const surname = prompt("Unesite prezime: ");
+//     const profession = prompt("Unesite zanimanje: ");
+//     const salary = +prompt("Unesite plaću: ");
+
+//     const person = {
+//         name, surname, profession, salary
+//     };
+
+//     people.push(person);
+    
+
+//     let alreadyExists = professions.some(item => item.profession === profession);
+
+//     if (alreadyExists) {
+//         dictionary[profession].salary += salary;
+//         dictionary[profession].count++;
+//     }
+//     else
+//     {
+//         dictionary.push(
+//             name, surname, profession, salary
+//         )
+//     }
+    
+//     const continuation = confirm("Želite li nastaviti unos?");
+
+//     if (!continuation)
+//         break;
+// }
+// nedovršeno
+
+// 3. Isti unos kao u drugom zadatku. Treba izračunati zbroj svih 
+// plaća zajedno i ispisati objekt u kojem se nalazi ime zanimanja, 
+// postotak koliko to zanimanje pridonosi ukupnoj plaći, i nizu objekata 
+// koji se sastoje od imena osobe i postotak koliko ta osoba pridonosi 
+// ukupnoj plaći zanimanja
+
+//preskočeno
+
+//4. Unijeti niz voća sa imenom, bojom i kalorijama. Cilj je ispisati svo voće sa 
+// istom bojom i koliko ukupno kalorija to voće daje. Neka se sortira po imenu boje.
+
+const fruits = [];
+
+while (true) {
+    const name = prompt("Unesite ime voća");
+    const color = prompt("Unesite boju voća");
+    const calories = +prompt("Unesite broj kalorija");
+
+    let fruit = {
+        name: name,
+        color: color,
+        calories: calories
     };
+    fruits.push(fruit);
 
-    articles.push(article);
-
-    const continuation = confirm("Želite li nastaviti unos?");
-
-    if (continuation === false)
+    const continuation = confirm("Želite li nastavit unos?");
+    if (!continuation)
         break;
 }
 
-const sumOfAllPrices = articles.reduce((acc, value) => acc + value.price, 0);
-const averagePrice = (sumOfAllPrices / articles.length).toFixed(2);
+function groupFruitsAndShowCalories(fruitsArray) 
+{
 
+    const groupedFruits = {};
 
-let maxDeviation = 0;
-let maxDeviationArticle = articles[0];
-articles.forEach(a => {
-    const deviation = Math.abs(a.price - averagePrice);
+    fruitsArray.forEach(function(fruit)
+    {
+         
+        if (!groupedFruits[fruit.color])
+            groupedFruits[fruit.color] = [];
 
-    if (deviation > maxDeviation) {
-        maxDeviation = deviation;
-        maxDeviationArticle = a;
+        groupedFruits[fruit.color].push(fruit);
+    });
+
+    for (let color in groupedFruits)
+    {
+        let totalCalories = 0;
+
+        groupedFruits[color].forEach
+        (
+            function (fruit) 
+            {
+                totalCalories += fruit.calories;
+            }
+        )
+
+        console.log(color + " - " + totalCalories + " kalorija");
     }
-});
 
-console.log("Prosjek cijena: ", averagePrice);
-console.log("Najveće odstupanje: ", maxDeviationArticle, " (", maxDeviation.toFixed(2), " eura)");
+}
 
+groupFruitsAndShowCalories(fruits);
